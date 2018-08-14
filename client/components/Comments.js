603283d1edb2f442051c357b2d129e29067d85e6
Vikas Shah <vikas.shah@essilorusa.com>
2018-08-14 10:20:29 -0400

    photo and comments update from actual code

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -2,33 +2,43 @@ import React from "react";
 
 const Comments = React.createClass({
   renderComment(comment, i) {
-    //console.log(comment);
-    return(
+    return (
       <div className="comment" key={i}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
+          <button
+            className="remove-comment"
+            //onClick={this.props.removeComment.bind(null,this.props.params.postId,i)}
+          >
+            &times;
+          </button>
         </p>
       </div>
-    )
+    );
   },
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
-    const {postId} = this.props.params;
-    const author= this.ref.author.value;
-    const comment= this.ref.comment.value;
-    console.log(postId,author,comment);
-    this.props.addComment(postId,author,comment);
+    this.props.addComment(
+      this.props.params.postId,
+      this.refs.author.value,
+      this.refs.comment.value
+    );
     this.refs.commentForm.reset();
   },
   render() {
+    const comments = this.props.comments[this.props.params.postId] || [];
     return (
-      <div className="comment">
-        {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit} >
-          <input type="text" ref="author" placeholder="author"/>
-          <input type="text" ref="comment" placeholder="comment"/>
+      <div className="comments">
+        {comments.map(this.renderComment)}
+
+        <form
+         // onSubmit={this.handleSubmit}
+          ref="commentForm"
+          className="comment-form"
+        >
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
         </form>
       </div>
--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,42 +1,48 @@
 import React from "react";
+import Comments from "./Comments";
+import CSSTransitionGroup from "react-addons-css-transition-group";
 import { Link } from "react-router";
-import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 const Photo = React.createClass({
-  
   render() {
+    console.log(this.props);
     const { post, i, comments } = this.props;
     return (
-      <figure className="grid-figure">
-        <div className="grid-photo-wrapper">
+      <figure key={i} className="grid-figure">
+        <div className="grid-photo-wrap">
           <Link to={`/view/${post.code}`}>
             <img
+              className="grid-photo"
               src={post.display_src}
               alt={post.caption}
-              className="grid-photo"
             />
           </Link>
-          <ReactCSSTransitionGroup
+
+          <CSSTransitionGroup
             transitionName="like"
             transitionEnterTimeout={500}
             transitionLeaveTimeout={500}
           >
-            <span key={post.likes} className="like-heart" />
-          </ReactCSSTransitionGroup>
+            <span key={post.likes} className="likes-heart">
+              {post.likes}
+            </span>
+          </CSSTransitionGroup>
         </div>
+
         <figcaption>
           <p>{post.caption}</p>
-          <div className="control-buttons">
 
+          <div className="control-buttons">
             <button
-              //onClick={this.props.increment.bind(null,i)}
+              //onClick={this.props.increment.bind(null, i)}
               className="likes"
             >
               &hearts; {post.likes}
             </button>
-            <Link className="button" to={`/view/${post.code}`}>
+
+            <Link to={`/view/${post.code}`} className="button">
               <span className="comment-count">
-                <span className="speech-bubble" />
+                <span className="speech-bubble" />{" "}
                 {comments[post.code] ? comments[post.code].length : 0}
               </span>
             </Link>
